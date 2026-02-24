489a7531341f8d43dee9fd67c39893436b68226a
Divyashree Mallarapu <mdivya0212@gmail.com>
2026-02-24 16:44:48 +0530

    Revise skills section with additional technologies
    Updated skills list to include new technologies and tools.

--- pages/4_Skills.py
+++ pages/4_Skills.py
@@ -23,10 +23,19 @@ def txt3(a, b):
 st.markdown(f'<p style="font-size: 20px;">Machine Learning, Deep Learning, Computer Vision, Docker, SQL</p>', unsafe_allow_html=True)
 
 txt3("Programming Languages", "`Python`, `C`, `HTML`, `CSS`, `JavaScript`")
-txt3("Developer Tools", "`VS Code`, `Git`, `GitHub`, `Google Cloud Platform (GCP)`, `Jupyter Notebook`, `PyCharm`")
-txt3("Technologies & Frameworks", "`TensorFlow`, `Azure`, `Keras`, `YOLO (v5, v8, v11)`, `SAM2`, `OpenCV`, `Hugging Face`, `Gradio`, `Streamlit`, `Google API`, `Gradle`, `Maven`")
-txt3("Database Management", "`MySQL`")
-txt3("Operating Systems", "`Windows`")
-txt3("Libraries", "`NumPy`, `Pandas`, `Matplotlib`, `Seaborn`, `Scikit-learn`, `Pillow`")
-txt3("Tools", "`LabelImg`")
-txt3("Soft Skills", "`Communication`, `Adaptability`, `Team Collaboration`, `Problem-Solving`, `Time Management`")
+
+txt3("Web & Application Development", "`React`, `Node.js`, `Express.js`, `Flask`, `Streamlit`, `Gradio`")
+
+txt3("Databases", "`MySQL`, `MongoDB`")
+
+txt3("Machine Learning & AI", "`TensorFlow`, `Keras`, `Scikit-learn`, `YOLO (v5, v8)`, `OpenCV`, `Hugging Face Transformers`")
+
+txt3("Data Analysis & Visualization", "`NumPy`, `Pandas`, `Matplotlib`, `Seaborn`")
+
+txt3("Version Control & Deployment", "`Git`, `GitHub`, `Streamlit Cloud`, `Railway`, `Render`, `Vercel`, `Hugging Face Spaces`")
+
+txt3("Cloud Platforms", "`Azure`, `Google Cloud Platform`")
+
+txt3("Developer Tools", "`VS Code`, `Jupyter Notebook`, `Collab Notebook`, `PyCharm`, `LabelImg`,`labelme`")
+
+txt3("Leadership & Activities", "`Hackathon Mentor`, `Final-year Project Lead`, `Workshop on GitHub & Deployment`")
